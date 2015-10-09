6ac35358507010083ad0d1886d674f0cb68d5b7c
lidasong <1584809032@qq.com>
2015-10-09 10:44:03 +0800

    队列

--- queue.js
+++ queue.js
@@ -4,6 +4,40 @@ function Queue(){
 	this.dequeue=dequeue;
 	this.front=front;
 	this.back=back;
-	this.valueOf=valueOf;
-	this.empty=empty;
+	this.toString=toString;
+	this.isEmpty=isEmpty;
+	this.size=size;
 }
+
+function size(){
+	return this.dataStore.length;
+}
+
+function enqueue(element){
+	this.dataStore.push(element);
+	return this.size();
+}
+
+function dequeue(){
+	return this.dataStore.shift();
+}
+
+function toString(){
+	var resultStr = this.dataStore.join('\n');
+	return resultStr;
+}
+
+function isEmpty(){
+	return this.size()===0;
+}
+
+function front(){
+	return this.size()?this.dataStore[0]:null;
+}
+
+function back(){
+	var size = this.size();
+	return size?this.dataStore[size-1]:null;
+}
+
+module.exports=Queue;
