021a636169e9869c83bec84a1f3f07a2bd480d71
lidasong <1584809032@qq.com>
2015-10-14 11:28:11 +0800

    完善图结构的深度优先遍历

--- graph.js
+++ graph.js
@@ -11,10 +11,63 @@ function Graph(v){
 	this.vertices = v;
 	this.edges = 0;
 	this.adj = [];
-	for(var i =0,verticesLen = vertices.length;i<verticesLen;i++){
-		this.adj[i] = [];
-		this.adj[i].push('');
-	}
+	this.marked = [];
 	this.addEdge = addEdge;
 	this.valueOf = valueOf;
+	this.showGraph = showGraph;
+	this.dfs = dfs;
+	constructorGraph.bind(this)();
 }
+
+function constructorGraph(){
+	for(var i =0;i<this.vertices;i++){
+		this.adj[i] = [];
+		// this.adj[i].push('');
+		this.marked[i] = false;
+	}
+}
+
+function addEdge(v,w){
+	try{
+		this.adj[v].push(w);
+		this.adj[w].push(v);
+		this.edges++;
+	}catch(e){
+		console.log(e);
+	}finally{
+		return this.edges;
+	}
+}
+
+function showGraph(){
+	for(var i = 0,v = this.vertices;i<v;i++){
+		var temp = [];
+		temp.push(i+'->');
+		for(var j = 0;j<v;j++){
+			if(this.adj[i][j]!==undefined){
+				temp.push(this.adj[i][j]);
+			}
+		}
+		console.log(temp.join(' '));
+	}
+}
+
+function valueOf(){
+
+}
+
+function dfs(v){
+	v = v || 0;
+	this.marked[v] = true;
+	if(this.adj[v]!==undefined){
+		console.log('visited vertex: '+v);
+	}
+	var temp = this.adj[v];
+	for(var i in temp){
+		if(!this.marked[temp[i]]){
+			this.dfs(temp[i]);
+		}
+	}
+}
+
+module.exports = Graph;
